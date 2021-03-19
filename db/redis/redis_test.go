a98fa7c1170dbaac25f08aedbf0122001d5d560f
crunchypi <crunchypi@protonmail.com>
2021-03-19 16:51:39 +0100

    add caching for ips (antispam/antidos mechanism with redis)

--- db/redis/redis_test.go
+++ db/redis/redis_test.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"testing"
+	"time"
 )
 
 var (
@@ -36,4 +37,37 @@ func TestSetGetLastQueryID(t *testing.T) {
 	if !ok || v != id {
 		t.Fatalf("unexpected query fail: %v, %v", v, ok)
 	}
+}
+
+func TestCheckRegDOSIP(t *testing.T) {
+	// # Backup pkg vars so it's safe to reduce
+	// # allowance and expiration (so test is quicker).
+	dguardExpBackup := dosguardExpiration
+	dguardAllowBackup := dosguardAllowance
+
+	ip := "0.0.0.0"
+	expire := time.Second * 3
+	allow := 2
+
+	dosguardExpiration = expire
+	dosguardAllowance = allow
+
+	// # Use up allowance.
+	for i := 0; i < allow; i++ {
+		if ok, err := r.CheckRegDOSIP(ip); !ok || err != nil {
+			t.Fatalf("checkreg step 1 (iter %v) fail: %v, %v", i, ok, err)
+		}
+	}
+	// # Exceed allowance
+	if ok, err := r.CheckRegDOSIP(ip); ok || err != nil {
+		t.Fatalf("checkreg step 2 fail: %v, %v", ok, err)
+	}
+	// # Wait until ip expires.
+	time.Sleep(expire + 1)
+	if ok, err := r.CheckRegDOSIP(ip); !ok || err != nil {
+		t.Fatalf("checkreg step 3 fail: %v, %v", ok, err)
+	}
+	// # Cleanup.
+	dosguardExpiration = dguardExpBackup
+	dosguardAllowance = dguardAllowBackup
 }
